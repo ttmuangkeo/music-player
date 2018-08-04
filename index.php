cbf966e235b0f0aa0fbf8e3692bd9a33d34f1236
ttmuangkeo <tim.muangkeo@gmail.com>
2018-08-03 23:25:51 -0700

    added nav menu

--- index.php
+++ index.php
@@ -24,7 +24,38 @@ else {
 <div id="mainContainer">
 
 <div id="topContainer">
-	
+
+	<div id="navBarContainer">
+		<nav class="navBar">
+
+			<a class="logo" href="index.php">
+				<img src="assets/images/icons/logo.png" alt="">
+			</a>
+
+			<div class="group">
+				<div class="navItem">
+					<a class="navItemLink" href="search.php">Search
+					<img class="icon" src="assets/images/icons/search.png" alt="">
+					</a>
+				</div>
+			</div>
+
+			<div class="group">
+				<div class="navItem">
+					<a class="navItemLink" href="browse.php">Browse</a>
+				</div>
+				<div class="navItem">
+					<a class="navItemLink" href="yourMusic.php">Your Music</a>
+				</div>
+				<div class="navItem">
+					<a class="navItemLink" href="profile.php">User's playlist</a>
+				</div>
+			</div>
+
+			
+		</nav>
+	</div>
+
 </div>
 
 <div id="nowPlayingBarContainer">
