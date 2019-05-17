0003a1c15f8ac312008d70257896c52348061add
ttmuangkeo <tim.muangkeo@gmail.com>
2019-05-16 22:51:23 -0700

    added songs

---
+++ includes/navBarContainer.php
@@ -0,0 +1,30 @@
+<div id="navBarContainer">
+	<nav class="navBar">
+
+		<a class="logo" href="index.php">
+			<img src="assets/images/icons/logo.png" alt="">
+		</a>
+
+		<div class="group">
+			<div class="navItem">
+				<a class="navItemLink" href="search.php">Search
+				<img class="icon" src="assets/images/icons/search.png" alt="">
+				</a>
+			</div>
+		</div>
+
+		<div class="group">
+			<div class="navItem">
+				<a class="navItemLink" href="browse.php">Browse</a>
+			</div>
+			<div class="navItem">
+				<a class="navItemLink" href="yourMusic.php">Your Music</a>
+			</div>
+			<div class="navItem">
+				<a class="navItemLink" href="profile.php">User's playlist</a>
+			</div>
+		</div>
+
+		
+	</nav>
+</div>
--- index.php
+++ index.php
@@ -1,159 +1,26 @@
-<?php
-include("includes/config.php");
+<?php include('includes/header.php') ?>
 
-//session_destroy(); LOGOUT
+<h1 class="pageHeadingBig">You Might Also Like</h1>
 
-if(isset($_SESSION['userLoggedIn'])) {
-	$userLoggedIn = $_SESSION['userLoggedIn'];
-}
-else {
-	header("Location: register.php");
-}
+<div class="gridViewContainer">
+	<?php 
+		$albumQuery = mysqli_query($con, "select * from Albums order by rand() limit 10");
+		while($row = mysqli_fetch_array($albumQuery)) {
+			echo '<div class="gridViewItem">
 
-?>
+				<img class="img-responsive" src="' . $row["artworkPath"] . '" alt="">
 
-<html>
-<head>
-	<title>Welcome to Slotify!</title>
-	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
-</head>
-
-<body>
-
-
-<div id="mainContainer">
-
-<div id="topContainer">
-
-	<div id="navBarContainer">
-		<nav class="navBar">
-
-			<a class="logo" href="index.php">
-				<img src="assets/images/icons/logo.png" alt="">
-			</a>
-
-			<div class="group">
-				<div class="navItem">
-					<a class="navItemLink" href="search.php">Search
-					<img class="icon" src="assets/images/icons/search.png" alt="">
-					</a>
-				</div>
-			</div>
-
-			<div class="group">
-				<div class="navItem">
-					<a class="navItemLink" href="browse.php">Browse</a>
-				</div>
-				<div class="navItem">
-					<a class="navItemLink" href="yourMusic.php">Your Music</a>
-				</div>
-				<div class="navItem">
-					<a class="navItemLink" href="profile.php">User's playlist</a>
-				</div>
-			</div>
-
-			
-		</nav>
-	</div>
-
-</div>
-
-<div id="nowPlayingBarContainer">
-		<div id="nowPlayingBars">
-
-		<div id="nowPlayingLeft">
-			<div class="content">
-				<span class="albumLink">
-					<img class="albumArtwork" src="https://i.ytimg.com/vi/rb8Y38eilRM/maxresdefault.jpg" alt="">
-				</span>
-
-				<div class="trackInfo">
-					
-					<span class="trackName">
-						<span>a millie</span>
-					</span>
-					<span class="artistName">
-						<span>lil wayne</span>
-					</span>
-				</div>
-
-
-
-
-
-
-			</div>
-			
-		</div>
-
-		<div id="nowPlayingCenter">
-			<div class="content playerControls">
-				
-				<div class="buttons">
-					<button class="controlButton shuffle" title="shuffle button">
-					<img src="assets/images/icons/shuffle.png" alt="shuffle">
-					</button>
-
-					<button class="controlButton prev" title="prev button">
-					<img src="assets/images/icons/prev.png" alt="prev">
-					</button>
-
-					<button class="controlButton play" title="play button">
-					<img src="assets/images/icons/play.png" alt="play">
-					</button>
-
-					<button class="controlButton pause" title="pause button" style="display:none;">
-					<img src="assets/images/icons/pause.png" alt="pause">
-					</button>
-
-					<button class="controlButton next" title="next button">
-					<img src="assets/images/icons/next.png" alt="next">
-					</button>
-
-					<button class="controlButton repeat" title="repeat button">
-					<img src="assets/images/icons/repeat.png" alt="repeat">
-					</button>
-
-				</div>
-
-
-				<div class="playbackBar">
-					<span class="progressTime current">0.00</span>
-					<div class="progressBar">
-						<div class="progressBarBg">
-							<div class="progress"></div>
-						</div>
-					</div>
-					<span class="progressTime remaining">0.00</span>
-
-
-				</div>
-
-			</div>
-			
-		</div>
-
-
-		<div id="nowPlayingRight">
-			<div class="volumeBar">
-				<button class="controlButton volume" title="Volume button">
-					<img src="assets/images/icons/volume.png" alt="Volume">
-				</button>
-
-				<div class="progressBar">
-					<div class="progressBarBg">
-						<div class="progress"></div>
-					</div>
-				</div>
-			</div>
-		</div>
-			
-		</div>
-	</div>	
+				<div class="gridViewInfo">'
+					. $row["title"];
+				'</div>
+			</div>';
+		} 
+	 ?>
 </div>
 
 
 
-</body>
 
-</html>
+
+
+<?php include('includes/footer.php') ?>
