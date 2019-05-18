58ce33b0c83cbebaab96c54eb9fe9bedec2bd029
ttmuangkeo <tim.muangkeo@gmail.com>
2019-05-17 22:36:45 -0700

    styled album page

--- album.php
+++ album.php
@@ -9,14 +9,18 @@ if(isset($_GET['id'])) {
 
 $album = new Album($con, $albumId);
 $artist = $album->getArtist();
-
-echo $album->getTitle() . '<br>';
-echo $artist->getName();
-
-
 ?>
 
+<div class="entityInfo">
+	<div class="leftSection">
+		<img src="<?php echo $album->getArtworkPath() ?>" alt="">
+	</div>
 
+	<div class="rightSection">
+		<h2><?php echo $album->getTitle(); ?></h2>
+		<span>By: <?php echo $artist->getName(); ?></span>
+	</div>
+</div>
 
 
 <?php include('includes/footer.php') ?>
